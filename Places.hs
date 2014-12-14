264e3d37c6c265e690d17a5db3c992b716c0f736
Libby H <libby@daydrea.me>
2014-12-14 10:17:36 -0500

    Changed my mind about the Sets thing.

--- Places.hs
+++ Places.hs
@@ -1,22 +1,20 @@
 module Places where
 
 import Dictionary (Direction)
-import Data.Set (Set, fromList)
-import qualified Data.Set as Set
 
 data Place =  Place { num          :: Int
                     , name         :: String
                     , description  :: String
-                    , exits        :: Set Exit
+                    , exits        :: [Exit]
                       } deriving Eq
 instance Show Place where
     show (Place _ name desc _) = name ++ "\n" ++ desc
 
 defaultPlace :: Place
-defaultPlace = Place 1 "A place" "Description of the place." (fromList [defaultExit])
+defaultPlace = Place 1 "A place" "Description of the place." [defaultExit]
 
 data Exit = Exit { direction :: Direction
-                 , synonyms  :: Set String
+                 , synonyms  :: [ String ]
                  , node      :: Int } deriving (Eq, Show, Ord)
 
-defaultExit = Exit "" Set.empty 1
+defaultExit = Exit "" [] 1
