ef258050a5a66c13a31f72794f6afc68d6752988
Libby H <libby@daydrea.me>
2014-12-13 20:21:01 -0500

    Changed lists of exits to sets. Added synonyms to definition of exits.

--- Places.hs
+++ Places.hs
@@ -1,11 +1,13 @@
 module Places where
 
 import Dictionary (Direction)
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 data Place =  Place { num          :: Int
                     , name         :: String
                     , description  :: String
-                    , exits        :: [Exit]
+                    , exits        :: Set Exit
                       } deriving Eq
 instance Show Place where
     show (Place _ name desc _) = name ++ "\n" ++ desc
@@ -14,6 +16,7 @@ defaultPlace :: Place
 defaultPlace = Place 1 "A place" "Description of the place." [defaultExit]
 
 data Exit = Exit { direction :: Direction
+                 , synonyms  :: Set String
                  , node      :: Int } deriving (Eq, Show)
 
 defaultExit = Exit "" 1
