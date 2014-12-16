9aabdcf05a43cebf5bf37ddf949f3270abcf8eb0
Libby H <libby@daydrea.me>
2014-12-15 20:06:21 -0500

    Added function to create dictionary.

--- Dictionary.hs
+++ Dictionary.hs
@@ -2,11 +2,19 @@ module Dictionary where
 
 import Data.Char (toLower)
 import Data.Map (Map, fromList, findWithDefault)
+import qualified Data.Map as Map
 import Text.ParserCombinators.Parsec 
 
 type Dictionary = Data.Map.Map UserInput Direction
 type UserInput = String
 type Direction = String
+
+toDictionary :: [ (Direction, [UserInput]) ] -> Dictionary
+toDictionary list = Map.fromList $ concatMap toDefinition list
+
+toDefinition :: (Direction, [UserInput]) -> [(UserInput, Direction)]
+toDefinition (x, []) = [(map toLower x, x)]
+toDefinition (x, y:ys) = (y, x) : toDefinition (x, ys)
 
 findDirection :: UserInput -> Dictionary -> Direction
 findDirection dir = findWithDefault "no match" (map toLower dir)
--- tests/HSpecTests.hs
+++ tests/HSpecTests.hs
@@ -17,45 +17,47 @@ sampleGraph = createGraph samplePlaces
 
 main :: IO()
 main = hspec $ do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
 
-  describe "Graph" $ do
+    describe "Graph" $ do
 
-    describe "nodeFromPlace" $ do
-      it "creates a node representing a place" $ do 
-        nodeFromPlace defaultPlace
-          `shouldBe` (1, defaultPlace)
+        describe "nodeFromPlace" $
+            it "creates a node representing a place" $ 
+                nodeFromPlace defaultPlace `shouldBe` (1, defaultPlace)
 
-    describe "edgesFromPlace" $ do
-      it "creates a list of edges from the list of exits from a place" $ do
-        edgesFromPlace defaultPlace `shouldBe` [(1, 1, "")]
+        describe "edgesFromPlace" $ 
+            it "creates a list of edges from the list of exits from a place" $ 
+                edgesFromPlace defaultPlace `shouldBe` [(1, 1, "")]
 
-    -- not sure how to test this function.
-    describe "createGraph" $ do
-      it "creates a graph from a list of places and directions" $ do
-        pendingWith "Dunno how to test this."
+        describe "createGraph" $ 
+            it "creates a graph from a list of places and directions" $ 
+                pendingWith "Dunno how to test this."
     
-    describe "maybeFindNode" $ do
-      it "finds a node by following direction from a list of edges" $ do
-        maybeFindNode "South" (out sampleGraph 1)
-          `shouldBe` Just 2  
-      it "return Nothing if no matching edge" $ do
-        maybeFindNode "Albequerque" (out sampleGraph 1)
-          `shouldBe` Nothing
+        describe "maybeFindNode" $ do
+            it "finds a node by following direction from a list of edges" $
+                maybeFindNode "South" (out sampleGraph 1)
+                    `shouldBe` Just 2  
+            it "return Nothing if no matching edge" $ 
+                maybeFindNode "Albequerque" (out sampleGraph 1)
+                    `shouldBe` Nothing
 
-    describe "findNodeByDirection" $ do
-      it "finds the place matching the exit direction" $ do
-        findNodeByDirection 1  "South" sampleGraph `shouldBe` 
-            Right 2
-      it "returns the old place if no matching edges" $ do
-        findNodeByDirection 1 "Clockwise" sampleGraph `shouldBe` 
-            Left "You can't go that way."
+        describe "findNodeByDirection" $ do
+            it "finds the place matching the exit direction" $ 
+                findNodeByDirection 1  "South" sampleGraph `shouldBe` 
+                    Right 2
+            it "returns the old place if no matching edges" $ 
+                findNodeByDirection 1 "Clockwise" sampleGraph `shouldBe` 
+                    Left "You can't go that way."
 
     describe "Dictionary" $ do
+
+        describe "toDefinition" $ do
+            it "converts an exit to a definition" $
+                toDefinition ("Down", ["d", "descend"]) `shouldBe`
+                    [("d","Down"),("descend","Down"),("down","Down")]
         
         describe "toDictionary" $ do
-            it "converts a list (directions, list of synonyms) to a dictionary" $ do
+            it "converts a list (directions, list of synonyms) to a dictionary" $ 
                 toDictionary [("South", ["s"])] `shouldBe` 
-                    Map.fromList [("s", "South")] 
+                    Map.fromList [("s", "South"), ("south", "South")]
+            it "should be empty given an empty list" $ 
+                toDictionary [] `shouldBe` Map.empty 
