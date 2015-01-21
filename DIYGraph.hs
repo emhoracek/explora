a79a5e677ecd0230d47b1618175622dc1de49d5d
Libby H <libby@daydrea.me>
2015-01-21 14:44:31 -0500

    added my own graph

---
+++ DIYGraph.hs
@@ -0,0 +1,72 @@
+module DIYGraph where
+
+type NodeID = Int
+
+type Node a = (NodeID, a)
+
+type Edge b = (NodeID, NodeID, b)
+
+type Links b = [(b, NodeID)]
+
+-- | Links to the node, the node, links from the node
+type Context a b = (Links b, Node a, Links b)
+
+data Graph a b = EmptyGraph | (:&:) (Context a b) (Graph a b) deriving Eq
+instance (Show a, Show b) => Show (Graph a b) where
+    show (context :&: graph) = show context ++ " & " ++
+                               show graph
+    show EmptyGraph = "EmptyGraph"
+instance Functor (Graph blah) where
+    fmap f EmptyGraph = EmptyGraph
+    fmap f ((linksA, node, linksB) :&: graph) = 
+        (mapLinks linksA, node, mapLinks linksB) :&: fmap f graph 
+        where mapLinks = map (\(a, b) -> (f a, b)) 
+
+singleton :: Node a -> Graph a ()
+singleton node = ([], node, []) :&: EmptyGraph
+
+insertNode :: Node a -> Graph a b -> Graph a b
+insertNode node graph = ([], node, []) :&: graph
+
+insertNodes :: [Node a] -> Graph a b -> Graph a b
+insertNodes [] graph = graph
+insertNodes [node] graph = insertNode node graph
+insertNodes (x:xs) graph = insertNode x (insertNodes xs graph)
+
+findNode :: NodeID -> Graph a b -> Bool
+findNode _ EmptyGraph = False
+findNode x ((_, (n, _), _) :&: graph) 
+    | x == n    = True
+    | otherwise = findNode x graph 
+
+removeNode :: (Eq a) => Node a -> Graph a b -> Graph a b
+removeNode _ EmptyGraph = EmptyGraph
+removeNode x graph@(context@(_, node, _) :&: g) 
+    | x == node          = g
+    | findNode (fst x) g = removeNode x g
+    | otherwise          = graph
+
+insertEdge :: Edge b -> Graph a b -> Graph a b
+insertEdge _ EmptyGraph = EmptyGraph
+insertEdge edge graph@(context :&: g) =
+    addEdgeToContext edge context :&: insertEdge edge g
+
+insertEdges :: [Edge b] -> Graph a b -> Graph a b
+insertEdges _ EmptyGraph = EmptyGraph
+insertEdges [] graph = graph
+insertEdges (edge:edges) graph = 
+    insertEdge edge (insertEdges edges graph)
+
+addEdgeToContext :: Edge b -> Context a b -> Context a b 
+addEdgeToContext (n1, n2, label) (linksA, node, linksB)
+    | n1 == fst node && n2 == fst node = 
+        ((label, n1) : linksA, node, (label, n2) : linksB)
+    | n1 == fst node = (linksA, node, (label, n2) : linksB)
+    | n2 == fst node = ((label, n1) : linksA, node, linksB)
+    | otherwise = (linksA, node, linksB)
+
+fromLists :: [Node a] -> [Edge b] -> Graph a b
+fromLists  nodes [] = insertNodes nodes EmptyGraph
+fromLists nodes edges = insertEdges edges $ insertNodes nodes EmptyGraph
+--fromLists (node@(id, _):xs) (edge@(n1, n2, _):ys) = 
+
---
+++ Test/DIYGraphSpec.hs
@@ -0,0 +1,91 @@
+module HSpecTests where
+
+import Test.Hspec
+import Test.QuickCheck
+import DIYGraph
+
+{--
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Graph a b) where
+    arbitrary = do
+        context <- arbitrary
+        graph   <- arbitrary
+    return $ oneOf [EmptyGraph, (context :&: graph)]
+--}
+
+sampleGraph :: Graph String String 
+sampleGraph = ([], (1, "hello"), []) :&: EmptyGraph
+
+sampleGraph2 :: Graph String ()
+sampleGraph2 = ([], (1, "hello"), []) :&: EmptyGraph
+
+sampleGraphInt :: Graph String Int
+sampleGraphInt = ([(2, 1)], (1, "hello"), [(3, 1)]) :&: EmptyGraph 
+
+main = hspec $ do
+    describe "fmap" $ do
+        it "satisfies law 1" $
+            fmap id sampleGraph `shouldBe` sampleGraph
+        it "satisfies law 2" $
+            fmap ((+2) . (* 4)) sampleGraphInt `shouldBe`
+                (fmap (+2) . fmap (*4)) sampleGraphInt
+    describe "singleton" $ 
+        it "is a single node with no edges" $
+            singleton (1, "hello") `shouldBe` sampleGraph2
+    
+    describe "insertNode" $ 
+        it "into an empty graph, same as singleton" $ property $
+            \x -> insertNode (x :: (Int, String)) EmptyGraph == singleton x
+
+    describe "insertNodes" $ do
+        context "empty list" $
+            it "returns the same graph" $ 
+               insertNodes [] sampleGraph `shouldBe` sampleGraph
+        context "single node" $
+            it "is the same as insertNode" $ property $
+                \x -> insertNodes [(x :: (Int, String))] sampleGraph
+                        == insertNode x sampleGraph
+        context "multiple nodes" $
+            it "should add the node to the graph" $
+                insertNodes [(3, "hello again"),
+                             (2, "goodbye")] sampleGraph `shouldBe`
+                    ([], (3, "hello again"), []) :&:
+                    (([], (2, "goodbye"), []) :&: sampleGraph)
+
+    describe "findNode" $ do
+        context "node in graph" $
+            it "says true" $
+                findNode 1 sampleGraph `shouldBe` True
+        context "not in graph" $
+            it "says false" $
+                findNode 32 sampleGraph `shouldBe` False
+
+    describe "removeNode" $ do
+        context "node in graph" $
+            it "removes node and context from graph" $
+                removeNode (1, "hello") sampleGraph `shouldBe`
+                    EmptyGraph
+        context "node not in graph" $
+            it "returns the same graph" $
+                removeNode (12, "yo") sampleGraph `shouldBe`
+                    sampleGraph
+
+    describe "insertEdge" $ do
+        context "empty graph" $
+            it "returns an empty graph" $
+                insertEdge (1,2,3) EmptyGraph `shouldBe` (EmptyGraph :: Graph Int Int)
+        context "no matching node" $
+            it "returns the same graph" $
+                insertEdge (12,43,34) sampleGraphInt `shouldBe` 
+                    sampleGraphInt
+        context "nodes match" $ 
+            it "add to the context of matching node" $
+                insertEdge (1,1,"turn around") sampleGraph `shouldBe`
+                    ([("turn around", 1)], (1, "hello"), [("turn around", 1)])
+                        :&: EmptyGraph
+
+    describe "fromLists" $
+        it "turns lists of nodes and lists of edges into a graph" $
+            fromLists [(1, "hello"), (2, "goodbye")] 
+                      [(1, 2, "exit")] `shouldBe`
+            (([], (1, "hello"), [("exit", 2)]) :&:
+             (([("exit", 1)], (2, "goodbye"), []) :&: EmptyGraph))
