cd8c008ca735d5bb06a21b4f277b2ce6f8b76b44
Libby H <libby@daydrea.me>
2015-01-31 21:27:09 -0500

    Fixed the HPC in the testing script (now it should work).

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
-runhaskell -itest-suite/ -itest-suite/librarySpec -itest-suite/executableSpec -iexecutable -ilibrary test-suite/Spec.hs
-echo "HPC tests:"
-hpc report tests.tix
+echo "Compiling test suite with HPC."
+ghc -fhpc -itest-suite/ -itest-suite/librarySpec -itest-suite/executableSpec -iexecutable -ilibrary test-suite/Spec.hs
+
+echo "Running test suite."
+./test-suite/Spec
+
+echo "\nHPC tests:"
+hpc report Spec.tix
+
+echo "\nRemoving tix file."
+rm Spec.tix
